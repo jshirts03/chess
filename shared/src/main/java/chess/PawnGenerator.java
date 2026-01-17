e0277ddec3586b395ab361bda1668ad7665944af
Jonah Shirts <jshirts03@gmail.com>
2026-01-16 23:02:20 -0700

    PawnGenerator special diagonal functions because of special movement rules

--- shared/src/main/java/chess/PawnGenerator.java
+++ shared/src/main/java/chess/PawnGenerator.java
@@ -25,15 +25,63 @@ public class PawnGenerator extends MoveGenerator{
         }
         return new ChessMove(myPosition, forwardPosition);
     }
+
+    //Ovverride because it can only move diagonally if it is killing an enemy pawn
+    @Override
+    public ChessMove checkDiagonalRight(ChessPosition mover){
+        ChessPosition diagonalPosition = new ChessPosition(mover.getRow()+1, mover.getColumn()+1);
+        if (diagonalPosition.getRow() < 1 || diagonalPosition.getRow() > 8){
+            return null;
+        }
+        if (diagonalPosition.getColumn() < 1 || diagonalPosition.getColumn() > 8){
+            return null;
+        }
+        if (board.getPiece(diagonalPosition).getTeamColor() != ChessGame.TeamColor.BLACK){
+            return null;
+        }
+        return new ChessMove(myPosition, diagonalPosition);
+    }
+
+    //Override because pawns can only move diagonal if they are killing enemy piece
+    @Override
+    public ChessMove checkDiagonalLeft(ChessPosition mover){
+        ChessPosition diagonalPosition = new ChessPosition(mover.getRow()+1, mover.getColumn()-1);
+        if (diagonalPosition.getRow() < 1 || diagonalPosition.getRow() > 8){
+            return null;
+        }
+        if (diagonalPosition.getColumn() < 1 || diagonalPosition.getColumn() > 8){
+            return null;
+        }
+        if (board.getPiece(diagonalPosition).getTeamColor() != ChessGame.TeamColor.BLACK){
+            return null;
+        }
+        return new ChessMove(myPosition, diagonalPosition);
+    }
+
+
+
+
     // Generate moves
     Collection<ChessMove> getMoves(){
         ChessPosition mover = myPosition;
 
-        ChessMove nextMove = checkForward(mover);
+        //Checks the diagonals
+        ChessMove nextMove = checkDiagonalLeft(mover);
+        if (nextMove != null){
+            moves.add(nextMove);
+        }
+        nextMove = checkDiagonalRight(mover);
         if (nextMove != null){
             moves.add(nextMove);
         }
 
+        //Checks 1 forward
+        nextMove = checkForward(mover);
+        if (nextMove != null){
+            moves.add(nextMove);
+        }
+
+        //If it's in starting row can move 2 forward
         if (myPosition.getRow()==2){
             mover = new ChessPosition(mover.getRow()+1, mover.getColumn());
             nextMove = checkForward(mover);
