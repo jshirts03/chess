3bc8ba893f3456c3fa30fdb122d24ba73204588f
Jonah Shirts <jshirts03@gmail.com>
2026-01-17 17:04:01 -0700

    BishopGenerator class created

---
+++ shared/src/main/java/chess/BishopGenerator.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class BishopGenerator extends MoveGenerator{
+
+    BishopGenerator(ChessBoard board, ChessPosition position){
+        super(board, position);
+    }
+
+
+}
