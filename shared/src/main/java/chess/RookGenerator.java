e1317274bd984fc93fea01c77342e792be631a39
Jonah Shirts <jshirts03@gmail.com>
2026-01-17 21:12:25 -0700

    RookGenerator created getMoves that checks front, back, left, right

--- shared/src/main/java/chess/RookGenerator.java
+++ shared/src/main/java/chess/RookGenerator.java
@@ -12,5 +12,57 @@ public class RookGenerator extends MoveGenerator {
         ChessMove nextMove = null;
 
         //Check front
+        do{
+            nextMove = checkForward(mover);
+            if (nextMove != null){
+                moves.add(nextMove);
+                mover = nextMove.endPosition;
+                if (isCapture(nextMove)){    // stops the loop if move captures an enemy piece
+                    nextMove = null;
+                }
+            }
+        }while(nextMove != null);
+
+        //Left
+        mover = myPosition;
+        do{
+            nextMove = checkLeft(mover);
+            if (nextMove != null){
+                moves.add(nextMove);
+                mover = nextMove.endPosition;
+                if (isCapture(nextMove)){    // stops the loop if move captures an enemy piece
+                    nextMove = null;
+                }
+            }
+        }while(nextMove != null);
+
+        //Right
+        mover = myPosition;
+        do{
+            nextMove = checkRight(mover);
+            if (nextMove != null){
+                moves.add(nextMove);
+                mover = nextMove.endPosition;
+                if (isCapture(nextMove)){    // stops the loop if move captures an enemy piece
+                    nextMove = null;
+                }
+            }
+        }while(nextMove != null);
+
+        //Backwards
+        mover = myPosition;
+        do{
+            nextMove = checkBackward(mover);
+            if (nextMove != null){
+                moves.add(nextMove);
+                mover = nextMove.endPosition;
+                if (isCapture(nextMove)){    // stops the loop if move captures an enemy piece
+                    nextMove = null;
+                }
+            }
+        }while(nextMove != null);
+
+        return moves;
+
     }
 }
