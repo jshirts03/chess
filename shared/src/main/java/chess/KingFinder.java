567c0b016ff9986d507f3d2b31856c3ea840c253
Jonah Shirts <jshirts03@gmail.com>
2026-02-02 17:26:47 -0700

    ChessGame, KingFinder class implemented to locate the current team's king

---
+++ shared/src/main/java/chess/KingFinder.java
@@ -0,0 +1,28 @@
+package chess;
+
+public class KingFinder {
+    private ChessBoard board;
+    private ChessGame.TeamColor teamColor;
+
+    public KingFinder(ChessGame.TeamColor teamColor, ChessBoard board){
+        this.board = board;
+        this.teamColor = teamColor;
+    }
+
+    boolean isKing(ChessPiece piece){
+        return (piece.type == ChessPiece.PieceType.KING && piece.color == teamColor);
+    }
+
+    ChessPosition getKing(){
+        for (int i=1; i < 9; i++){
+            for (int j=1; j < 9; j++){
+                ChessPosition searcher = new ChessPosition(i, j);
+                ChessPiece searchPiece = board.getPiece(searcher);
+                if (isKing(searchPiece)){
+                    return searcher;
+                }
+            }
+        }
+        return null;
+    }
+}
