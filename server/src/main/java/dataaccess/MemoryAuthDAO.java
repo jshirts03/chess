46426831c5a4db8b91f6b90901748e877a3cdf9f
Jonah Shirts <jshirts03@gmail.com>
2026-02-23 13:29:08 -0700

    Seting up AuthDAO and UserDAO constructors and clear methods

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,15 @@
 package dataaccess;
 
+import datatypes.AuthData;
+import java.util.HashSet;
+
 public class MemoryAuthDAO implements AuthDAO{
+    HashSet<AuthData> authSet;
+    public MemoryAuthDAO(){
+        authSet = new HashSet<AuthData>();
+    }
+
+    public void clear(){
+        authSet.clear();
+    }
 }
--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -3,14 +3,18 @@ package dataaccess;
 
 import datatypes.UserData;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class MemoryUserDAO implements UserDAO{
-    private ArrayList<UserData> users;
+    private HashSet<UserData> users;
 
-    MemoryUserDAO(){
-        users = new ArrayList<UserData>();
+    public MemoryUserDAO(){
+        users = new HashSet<UserData>();
     }
 
+    public void clear(){
+        users.clear();
+    }
     //insert methods here that manipulate the list of users stored in this DAO object
     //because there no DB, we just create one of these when the server starts up and then it will reset
 }
