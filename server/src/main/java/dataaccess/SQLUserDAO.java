3e36c85992e429fcfcb3246efaa4d8722dd95941
Jonah Shirts <jshirts03@gmail.com>
2026-03-11 00:23:21 -0600

    SQL UserDAO, getUser method implemented and works great

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,7 +1,12 @@
 package dataaccess;
 
+import datatypes.AuthData;
 import datatypes.UserData;
 import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class SQLUserDAO implements UserDAO {
 
@@ -19,7 +24,32 @@ public class SQLUserDAO implements UserDAO {
         }
     };
 
-    public UserData getUser(String username){return null;};
+    public UserData getUser(String username, String password){
+        String statement = String.format("SELECT username, password, email FROM chess.users WHERE username = '%s'", username);
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()){
+                        String dbUsername = rs.getString("username");
+                        String dbPassword = rs.getString("password");
+                        String email = rs.getString("email");
+                        if (BCrypt.checkpw(password, dbPassword)){
+                            return new UserData(dbUsername, password, email);
+                        }
+                        else{
+                            return null;
+                        }
+                    }
+                    else{
+                        return null;
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    };
 
 
     public void createUser(UserData user){
