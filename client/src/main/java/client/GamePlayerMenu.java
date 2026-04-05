9d5601f20fd187793f69876f14e1ae9fb587cbe9
Jonah Shirts <jshirts03@gmail.com>
2026-04-04 23:43:57 -0600

    Phase 6: Client setup: plan complete for client. I finally have the whole picture in my brian. I have the project decomposed enough for my brain to understand. I feel confident that I can do this!!

--- client/src/main/java/client/GamePlayerMenu.java
+++ client/src/main/java/client/GamePlayerMenu.java
@@ -1,6 +1,7 @@
 package client;
 
 public class GamePlayerMenu {
+    NotificationHandler notificationHandler;
     WebSocketFacade webSocketF;
     int gameId;
     int authToken;
@@ -9,6 +10,8 @@ public class GamePlayerMenu {
     public GamePlayerMenu(int gameId, int authToken){
         this.gameId = gameId;
         this.authToken = authToken;
+        this.notificationHandler = new NotificationHandler();
+        this.webSocketF = new WebSocketFacade(notificationHandler);
     }
 
     public void run(){
@@ -18,16 +21,27 @@ public class GamePlayerMenu {
         //you don't need to know if it's your turn on the client side. that is stored in the chess game in the server
         //Just loop through the UI options and the server will send back an error message if it's not your turn or if move is invalid
         //this class only will run the
+
         //help
         //prints help instructions
 
         //redraw board
-        //calls websocketF to
+        //calls the notificationHandler to print the board (done in the BoardPrinter)
+
         //make move
+        //calls the webSocket Facade to make a move with the coordinates
+        //notificationHandler will send back an error message if invalid move or if not their turn
+
         //highlight legal moves
+        //calls the notificationHandler to print the board, highlighting the legal moves (done in the BoardPrinter)
+
         //resign
+        //calls the websocketFacade to resign the game, does not leave the gamePlayer Menu
+
         //leave
-        webSocketF.game
+        //calls the websocketFacade to leave the game, closes websocket connection
+        //exit gamePlayer menu
+
 
     }
 }
--- client/src/main/java/client/WebSocketFacade.java
+++ client/src/main/java/client/WebSocketFacade.java
@@ -1,4 +1,9 @@
 package client;
 
 public class WebSocketFacade {
+    NotificationHandler notificationHandler;
+
+    public WebSocketFacade(NotificationHandler nf){
+        notificationHandler = nf;
+    }
 }
