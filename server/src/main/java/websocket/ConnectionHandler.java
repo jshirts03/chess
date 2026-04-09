29086b65436a0aaabf36413afb92e27706d5eb78
Jonah Shirts <jshirts03@gmail.com>
2026-04-09 00:32:53 -0600

    Phase 6: Server: Edited connectionHandler / makeMove to send a message to everyone if a user is in check after a move

--- server/src/main/java/websocket/ConnectionHandler.java
+++ server/src/main/java/websocket/ConnectionHandler.java
@@ -82,6 +82,25 @@ public class ConnectionHandler {
         sendNotification(session, builder.toString(), gameId);
     }
 
+    public void sendIsInCheckNotification(int gameId, ChessGame.TeamColor teamColor){
+        var playersInfo = sessions.get(gameId);
+        String inCheckUser = "";
+        String color;
+        for (ConnectionInfo info: playersInfo){
+            if (info.teamColor().equals(teamColor)){
+                inCheckUser = info.username();
+            }
+        }
+        if (teamColor.equals(ChessGame.TeamColor.WHITE)){
+            color = "WHITE";
+        }
+        else{
+            color = "BLACK";
+        }
+        String message = String.format("%s (%s) is in CHECK", inCheckUser, color);
+        sendNotification(null, message, gameId);
+    }
+
     public void sendGameOverNotification(ChessGame game, int gameId){
         String winner;
         if (game.getTeamTurn() == ChessGame.TeamColor.BLACK){
@@ -185,6 +204,12 @@ public class ConnectionHandler {
             //notify users
             sendAllLoadGame(gameId);
             sendMoveNotification(move, username, gameId, session);
+            if (game.isInCheck(ChessGame.TeamColor.WHITE)){
+                sendIsInCheckNotification(gameId, ChessGame.TeamColor.WHITE);
+            }
+            if (game.isInCheck(ChessGame.TeamColor.BLACK)){
+                sendIsInCheckNotification(gameId, ChessGame.TeamColor.BLACK);
+            }
             if (game.getGameIsOver()){
                 sendGameOverNotification(game, gameId);
             }
